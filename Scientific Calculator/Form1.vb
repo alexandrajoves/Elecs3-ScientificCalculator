f0b6faa9224bc0b60944d2706ad9e805092a5422
Alexandra Joves <49018999+alexandrajoves@users.noreply.github.com>
2019-11-23 01:31:51 +0800

    Add files via upload

---
+++ Scientific Calculator/Form1.vb
@@ -0,0 +1,158 @@
+﻿Public Class clear
+
+    Private Sub numberzero_Click(sender As Object, e As EventArgs) Handles numberzero.Click
+        this.textBox1.Text = "";
+        Input += "0";
+        this.textBox1.Text
+
+    End Sub
+
+    Private Sub equals_Click(sender As Object, e As EventArgs) Handles equals.Click
+        operand2 = Input();
+            Double num1, num2;
+            Double.TryParse(operand1, out num1);
+            Double.TryParse(operand2, out num2);
+
+            If (operation == '+')
+            {
+                result = num1 + num2;
+                TextBox1.Text = result.ToString();
+            }
+            ElseIf (operation == '-')
+            {
+                result = num1 - num2;
+                TextBox1.Text = result.ToString();
+            }
+            ElseIf (operation == '*')
+            {
+                result = num1 * num2;
+                TextBox1.Text = result.ToString();
+            }
+            ElseIf (operation == '/')
+            {
+                If(num2!= 0)
+                {
+                    result = num1 / num2;
+                    TextBox1.Text = result.ToString();
+                }
+                Else
+                {
+                    TextBox1.Text = "DIV/Zero!";
+                }
+
+            }
+    End Sub
+
+    Private Sub number1_Click(sender As Object, e As EventArgs) Handles number1.Click
+        this.textBox1.Text = "";
+        Input += "1";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number2_Click(sender As Object, e As EventArgs) Handles number2.Click
+        this.textBox1.Text = "";
+        Input += "2";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number3_Click(sender As Object, e As EventArgs) Handles number3.Click
+        this.textBox1.Text = "";
+        Input += "3";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number4_Click(sender As Object, e As EventArgs) Handles number4.Click
+        this.textBox1.Text = "";
+        Input += "4";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number5_Click(sender As Object, e As EventArgs) Handles number5.Click
+        this.textBox1.Text = "";
+        Input += "5";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number6_Click(sender As Object, e As EventArgs) Handles number6.Click
+        this.textBox1.Text = "";
+        Input += "6";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub clear_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub number7_Click(sender As Object, e As EventArgs) Handles number7.Click
+        this.textBox1.Text = "";
+        Input += "7";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number8_Click(sender As Object, e As EventArgs) Handles number8.Click
+        this.textBox1.Text = "";
+        Input += "8";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub number9_Click(sender As Object, e As EventArgs) Handles number9.Click
+        this.textBox1.Text = "";
+        Input += "9";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub plus_Click(sender As Object, e As EventArgs) Handles plus.Click
+        operand1 = Input();
+            operation = '+';
+            Input() = String.Empty;
+
+    End Sub
+
+    Private Sub minus_Click(sender As Object, e As EventArgs) Handles minus.Click
+        operand1 = Input();
+            operation = '-';
+            Input() = String.Empty;
+    End Sub
+
+    Private Sub multiply_Click(sender As Object, e As EventArgs) Handles multiply.Click
+        operand1 = Input();
+            operation = '*';
+            Input() = String.Empty;
+    End Sub
+
+    Private Sub sin_Click(sender As Object, e As EventArgs) Handles sin.Click
+
+    End Sub
+
+    Private Sub cos_Click(sender As Object, e As EventArgs) Handles cos.Click
+
+    End Sub
+
+    Private Sub log_Click(sender As Object, e As EventArgs) Handles log.Click
+
+    End Sub
+
+    Private Sub tan_Click(sender As Object, e As EventArgs) Handles tan.Click
+
+    End Sub
+
+    Private Sub divide_Click(sender As Object, e As EventArgs) Handles divide.Click
+        operand1 = Input();
+            operation = '/';
+            Input() = String.Empty;
+    End Sub
+
+    Private Sub point_Click(sender As Object, e As EventArgs) Handles point.Click
+        this.textBox1.Text = "";
+        Input += ".";
+        this.textBox1.Text
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles clr.Click
+        this.textBox1.Text = "";
+            this.input = String.Empty;
+            this.operand1 = String.Empty;
+            this.operand2 = String.Empty;
+
+    End Sub
+End Class
---
+++ Scientific Calculator/SciCal.Designer.vb
@@ -0,0 +1,325 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class clear
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.number1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.equals = New System.Windows.Forms.Button()
+        Me.plus = New System.Windows.Forms.Button()
+        Me.number2 = New System.Windows.Forms.Button()
+        Me.number3 = New System.Windows.Forms.Button()
+        Me.numberzero = New System.Windows.Forms.Button()
+        Me.point = New System.Windows.Forms.Button()
+        Me.clr = New System.Windows.Forms.Button()
+        Me.number4 = New System.Windows.Forms.Button()
+        Me.number5 = New System.Windows.Forms.Button()
+        Me.number6 = New System.Windows.Forms.Button()
+        Me.number7 = New System.Windows.Forms.Button()
+        Me.number8 = New System.Windows.Forms.Button()
+        Me.number9 = New System.Windows.Forms.Button()
+        Me.minus = New System.Windows.Forms.Button()
+        Me.multiply = New System.Windows.Forms.Button()
+        Me.divide = New System.Windows.Forms.Button()
+        Me.sin = New System.Windows.Forms.Button()
+        Me.cos = New System.Windows.Forms.Button()
+        Me.log = New System.Windows.Forms.Button()
+        Me.tan = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'number1
+        '
+        Me.number1.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number1.Location = New System.Drawing.Point(25, 219)
+        Me.number1.Name = "number1"
+        Me.number1.Size = New System.Drawing.Size(63, 45)
+        Me.number1.TabIndex = 0
+        Me.number1.Text = "1"
+        Me.number1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(301, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'equals
+        '
+        Me.equals.Location = New System.Drawing.Point(43, 321)
+        Me.equals.Name = "equals"
+        Me.equals.Size = New System.Drawing.Size(236, 40)
+        Me.equals.TabIndex = 13
+        Me.equals.Text = "="
+        Me.equals.UseVisualStyleBackColor = True
+        '
+        'plus
+        '
+        Me.plus.Location = New System.Drawing.Point(239, 117)
+        Me.plus.Name = "plus"
+        Me.plus.Size = New System.Drawing.Size(75, 45)
+        Me.plus.TabIndex = 14
+        Me.plus.Text = "+"
+        Me.plus.UseVisualStyleBackColor = True
+        '
+        'number2
+        '
+        Me.number2.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number2.Location = New System.Drawing.Point(94, 219)
+        Me.number2.Name = "number2"
+        Me.number2.Size = New System.Drawing.Size(63, 45)
+        Me.number2.TabIndex = 18
+        Me.number2.Text = "2"
+        Me.number2.UseVisualStyleBackColor = False
+        '
+        'number3
+        '
+        Me.number3.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number3.Location = New System.Drawing.Point(163, 219)
+        Me.number3.Name = "number3"
+        Me.number3.Size = New System.Drawing.Size(63, 45)
+        Me.number3.TabIndex = 19
+        Me.number3.Text = "3"
+        Me.number3.UseVisualStyleBackColor = False
+        '
+        'numberzero
+        '
+        Me.numberzero.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.numberzero.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.numberzero.Location = New System.Drawing.Point(25, 270)
+        Me.numberzero.Name = "numberzero"
+        Me.numberzero.Size = New System.Drawing.Size(63, 45)
+        Me.numberzero.TabIndex = 20
+        Me.numberzero.Text = "0"
+        Me.numberzero.UseVisualStyleBackColor = False
+        '
+        'point
+        '
+        Me.point.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.point.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.point.Location = New System.Drawing.Point(94, 270)
+        Me.point.Name = "point"
+        Me.point.Size = New System.Drawing.Size(63, 45)
+        Me.point.TabIndex = 21
+        Me.point.Text = "."
+        Me.point.UseVisualStyleBackColor = False
+        '
+        'clr
+        '
+        Me.clr.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.clr.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.clr.Location = New System.Drawing.Point(163, 270)
+        Me.clr.Name = "clr"
+        Me.clr.Size = New System.Drawing.Size(63, 45)
+        Me.clr.TabIndex = 22
+        Me.clr.Text = "CLR"
+        Me.clr.UseVisualStyleBackColor = False
+        '
+        'number4
+        '
+        Me.number4.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number4.Location = New System.Drawing.Point(25, 168)
+        Me.number4.Name = "number4"
+        Me.number4.Size = New System.Drawing.Size(63, 45)
+        Me.number4.TabIndex = 23
+        Me.number4.Text = "4"
+        Me.number4.UseVisualStyleBackColor = False
+        '
+        'number5
+        '
+        Me.number5.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number5.Location = New System.Drawing.Point(94, 168)
+        Me.number5.Name = "number5"
+        Me.number5.Size = New System.Drawing.Size(63, 45)
+        Me.number5.TabIndex = 24
+        Me.number5.Text = "5"
+        Me.number5.UseVisualStyleBackColor = False
+        '
+        'number6
+        '
+        Me.number6.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number6.Location = New System.Drawing.Point(163, 168)
+        Me.number6.Name = "number6"
+        Me.number6.Size = New System.Drawing.Size(63, 45)
+        Me.number6.TabIndex = 25
+        Me.number6.Text = "6"
+        Me.number6.UseVisualStyleBackColor = False
+        '
+        'number7
+        '
+        Me.number7.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number7.Location = New System.Drawing.Point(24, 117)
+        Me.number7.Name = "number7"
+        Me.number7.Size = New System.Drawing.Size(63, 45)
+        Me.number7.TabIndex = 26
+        Me.number7.Text = "7"
+        Me.number7.UseVisualStyleBackColor = False
+        '
+        'number8
+        '
+        Me.number8.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number8.Location = New System.Drawing.Point(94, 117)
+        Me.number8.Name = "number8"
+        Me.number8.Size = New System.Drawing.Size(63, 45)
+        Me.number8.TabIndex = 27
+        Me.number8.Text = "8"
+        Me.number8.UseVisualStyleBackColor = False
+        '
+        'number9
+        '
+        Me.number9.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.number9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.number9.Location = New System.Drawing.Point(163, 117)
+        Me.number9.Name = "number9"
+        Me.number9.Size = New System.Drawing.Size(63, 45)
+        Me.number9.TabIndex = 28
+        Me.number9.Text = "9"
+        Me.number9.UseVisualStyleBackColor = False
+        '
+        'minus
+        '
+        Me.minus.Location = New System.Drawing.Point(239, 168)
+        Me.minus.Name = "minus"
+        Me.minus.Size = New System.Drawing.Size(75, 45)
+        Me.minus.TabIndex = 29
+        Me.minus.Text = "-"
+        Me.minus.UseVisualStyleBackColor = True
+        '
+        'multiply
+        '
+        Me.multiply.Location = New System.Drawing.Point(239, 219)
+        Me.multiply.Name = "multiply"
+        Me.multiply.Size = New System.Drawing.Size(75, 45)
+        Me.multiply.TabIndex = 30
+        Me.multiply.Text = "x"
+        Me.multiply.UseVisualStyleBackColor = True
+        '
+        'divide
+        '
+        Me.divide.Location = New System.Drawing.Point(239, 270)
+        Me.divide.Name = "divide"
+        Me.divide.Size = New System.Drawing.Size(75, 45)
+        Me.divide.TabIndex = 31
+        Me.divide.Text = "/"
+        Me.divide.UseVisualStyleBackColor = True
+        '
+        'sin
+        '
+        Me.sin.Location = New System.Drawing.Point(11, 66)
+        Me.sin.Name = "sin"
+        Me.sin.Size = New System.Drawing.Size(70, 45)
+        Me.sin.TabIndex = 32
+        Me.sin.Text = "sin"
+        Me.sin.UseVisualStyleBackColor = True
+        '
+        'cos
+        '
+        Me.cos.Location = New System.Drawing.Point(87, 66)
+        Me.cos.Name = "cos"
+        Me.cos.Size = New System.Drawing.Size(70, 45)
+        Me.cos.TabIndex = 33
+        Me.cos.Text = "cos"
+        Me.cos.UseVisualStyleBackColor = True
+        '
+        'log
+        '
+        Me.log.Location = New System.Drawing.Point(163, 66)
+        Me.log.Name = "log"
+        Me.log.Size = New System.Drawing.Size(70, 45)
+        Me.log.TabIndex = 34
+        Me.log.Text = "log"
+        Me.log.UseVisualStyleBackColor = True
+        '
+        'tan
+        '
+        Me.tan.Location = New System.Drawing.Point(239, 66)
+        Me.tan.Name = "tan"
+        Me.tan.Size = New System.Drawing.Size(74, 45)
+        Me.tan.TabIndex = 35
+        Me.tan.Text = "tan"
+        Me.tan.UseVisualStyleBackColor = True
+        '
+        'clear
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(325, 367)
+        Me.Controls.Add(Me.tan)
+        Me.Controls.Add(Me.log)
+        Me.Controls.Add(Me.cos)
+        Me.Controls.Add(Me.sin)
+        Me.Controls.Add(Me.divide)
+        Me.Controls.Add(Me.multiply)
+        Me.Controls.Add(Me.minus)
+        Me.Controls.Add(Me.number9)
+        Me.Controls.Add(Me.number8)
+        Me.Controls.Add(Me.number7)
+        Me.Controls.Add(Me.number6)
+        Me.Controls.Add(Me.number5)
+        Me.Controls.Add(Me.number4)
+        Me.Controls.Add(Me.clr)
+        Me.Controls.Add(Me.point)
+        Me.Controls.Add(Me.numberzero)
+        Me.Controls.Add(Me.number3)
+        Me.Controls.Add(Me.number2)
+        Me.Controls.Add(Me.plus)
+        Me.Controls.Add(Me.equals)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.number1)
+        Me.Name = "clear"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents number1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents equals As Button
+    Friend WithEvents plus As Button
+    Friend WithEvents number2 As Button
+    Friend WithEvents number3 As Button
+    Friend WithEvents numberzero As Button
+    Friend WithEvents point As Button
+    Friend WithEvents clr As Button
+    Friend WithEvents number4 As Button
+    Friend WithEvents number5 As Button
+    Friend WithEvents number6 As Button
+    Friend WithEvents number7 As Button
+    Friend WithEvents number8 As Button
+    Friend WithEvents number9 As Button
+    Friend WithEvents minus As Button
+    Friend WithEvents multiply As Button
+    Friend WithEvents divide As Button
+    Friend WithEvents sin As Button
+    Friend WithEvents cos As Button
+    Friend WithEvents log As Button
+    Friend WithEvents tan As Button
+End Class
